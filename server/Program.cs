33240311722a2d2ada326f43153aec238f1f3cf3
Mike Harder <mharder@microsoft.com>
2020-02-06 12:59:51 -0800

    Add server app

---
+++ server/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    public class Program
+    {
+        private static readonly byte[] _payload = Encoding.UTF8.GetBytes("Hello, World!");
+
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, 5000);
+                })
+                .Configure(app => app.Run(context =>
+                {
+                    var payloadLength = _payload.Length;
+                    context.Response.StatusCode = 200;
+                    context.Response.ContentType = "text/plain";
+                    context.Response.ContentLength = payloadLength;
+                    return context.Response.Body.WriteAsync(_payload, 0, payloadLength);
+                }))
+                .Build()
+                .Run();
+        }
+    }
+}
