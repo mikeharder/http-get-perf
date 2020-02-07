c8829a40d43d7c97f805e09abafca653f0d0af12
Mike Harder <mharder@microsoft.com>
2020-02-06 18:08:04 -0800

    Add warmup and improve output

--- net-http-client/Program.cs
+++ net-http-client/Program.cs
@@ -15,35 +15,51 @@ namespace App
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("Usage: app <url> <parallel> <duration>");
+                Console.WriteLine("Usage: app <url> <parallel> <warmup> <duration>");
                 return;
             }
 
             var url = args[0];
             var parallel = args.Length >= 2 ? Int32.Parse(args[1]) : 32;
-            var duration = args.Length >= 3 ? Int32.Parse(args[2]) : 10;
+            var warmup = args.Length >= 3 ? Int32.Parse(args[2]) : 10;
+            var duration = args.Length >= 4 ? Int32.Parse(args[3]) : 10;
 
+            Console.WriteLine($"=== Parameters ===");
             Console.WriteLine($"Url: {url}");
             Console.WriteLine($"Parallel: {parallel}");
+            Console.WriteLine($"Warmup: {warmup}");
             Console.WriteLine($"Duration: {duration}");
             Console.WriteLine();
 
             var tasks = new Task[parallel];
-            var stopwatch = Stopwatch.StartNew();
-
             for (var i=0; i < parallel; i++)
             {
                 tasks[i] = ExecuteRequests(url);
             }
+
+            await CollectResults("Warmup", warmup);
+            await CollectResults("Test", duration);
+        }
+
+        private static async Task CollectResults(string title, int duration) {
+            Console.WriteLine($"=== {title} ===");
+            
+            var stopwatch = Stopwatch.StartNew();
+            Interlocked.Exchange(ref _completedRequests, 0);
+            
             await Task.Delay(TimeSpan.FromSeconds(duration));
-
+            
             stopwatch.Stop();
+            
+            PrintResults(_completedRequests, stopwatch.Elapsed);
+        }
 
-            var completedRequests = _completedRequests;
-            var elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
-            var requestsPerSecond = _completedRequests / elapsedSeconds;
+        private static void PrintResults(int completedRequests, TimeSpan elapsed) {
+            var elapsedSeconds = elapsed.TotalSeconds;
+            var requestsPerSecond = completedRequests / elapsedSeconds;
 
             Console.WriteLine($"Completed {completedRequests:N0} requests in {elapsedSeconds:N2} seconds ({requestsPerSecond:N0} req/s)");
+            Console.WriteLine();
         }
 
         private static async Task ExecuteRequests(string url)
