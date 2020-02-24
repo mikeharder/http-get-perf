3f5aac2887590da6d11f6134c96e8451cb6b0592
Mike Harder <mharder@microsoft.com>
2020-02-24 12:37:50 -0800

    [java-reactor-netty] Parse arguments and make single request

--- java-reactor-netty/src/main/java/com/mycompany/app/App.java
+++ java-reactor-netty/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,30 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import reactor.netty.http.client.HttpClient;
+
+public class App {
+    private static final HttpClient _httpClient = HttpClient.create();
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: app <url> <parallel> <warmup> <duration>");
+            return;
+        }
+
+        String url = args[0];
+        int parallel = args.length >= 2 ? Integer.parseInt(args[1]) : 64;
+        int warmup = args.length >= 3 ? Integer.parseInt(args[2]) : 30;
+        int duration = args.length >= 4 ? Integer.parseInt(args[3]) : 10;
+
+        System.out.println("=== Parameters ===");
+        System.out.println(String.format("Url: %s", url));
+        System.out.println(String.format("Parallel: %s", parallel));
+        System.out.println(String.format("Warmup: %s", warmup));
+        System.out.println(String.format("Duration: %s", duration));
+        System.out.println();
+
+        String response = _httpClient.get().uri(url).responseContent().aggregate().asString().block();
+
+        System.out.println(response);
     }
 }
