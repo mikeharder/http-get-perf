94709dce9db6b5c476506c8885229901c462ba57
Mike Harder <mharder@microsoft.com>
2020-07-21 11:32:44 -0700

    Respond to HTTP PUT

--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using System.Net;
 using System.Text;
 
@@ -8,6 +9,7 @@ namespace App
     public class Program
     {
         private static readonly byte[] _payload = Encoding.UTF8.GetBytes("Hello, World!");
+        private static readonly int _payloadLength = _payload.Length;
 
         public static void Main(string[] args)
         {
@@ -16,13 +18,39 @@ namespace App
                 {
                     options.Listen(IPAddress.Any, 5000);
                 })
-                .Configure(app => app.Run(context =>
+                .Configure(app => app.Run(async context =>
                 {
-                    var payloadLength = _payload.Length;
-                    context.Response.StatusCode = 200;
-                    context.Response.ContentType = "text/plain";
-                    context.Response.ContentLength = payloadLength;
-                    return context.Response.Body.WriteAsync(_payload, 0, payloadLength);
+                    if (context.Request.Method == HttpMethods.Put)
+                    {
+                        var reader = context.Request.BodyReader;
+                        long bytesRead = 0;
+                        while (true)
+                        {
+                            var result = await reader.ReadAsync();
+                            bytesRead += result.Buffer.Length;
+                            if (result.IsCompleted)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                reader.AdvanceTo(result.Buffer.End);
+                            }
+                        }
+
+                        var payload = Encoding.UTF8.GetBytes(bytesRead.ToString());
+                        context.Response.StatusCode = 200;
+                        context.Response.ContentType = "text/plain";
+                        context.Response.ContentLength = payload.Length;
+                        await context.Response.Body.WriteAsync(payload, 0, payload.Length);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 200;
+                        context.Response.ContentType = "text/plain";
+                        context.Response.ContentLength = _payloadLength;
+                        await context.Response.Body.WriteAsync(_payload, 0, _payloadLength);
+                    }
                 }))
                 .Build()
                 .Run();
