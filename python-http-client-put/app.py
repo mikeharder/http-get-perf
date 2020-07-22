411735cf3fe6aeb6762a68f23b2ce57e3e4e2922
Mike Harder <mharder@microsoft.com>
2020-07-21 18:11:44 -0700

    Python http client PUT test

---
+++ python-http-client-put/app.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import time
+from urllib.parse import urlparse
+import http.client
+
+class LargeStream:
+    def __init__(self, length, initial_buffer_length=1024*1024):
+        self._base_data = os.urandom(initial_buffer_length)
+        self._base_data_length = initial_buffer_length
+        self._position = 0
+        self._remaining = length
+
+    def read(self, size=None):
+        if self._remaining == 0:
+            return None
+
+        if size is None:
+            e = self._base_data_length
+        else:
+            e = size
+        e = min(e, self._remaining)
+        if e > self._base_data_length:
+            self._base_data = os.urandom(e)
+            self._base_data_length = e
+        self._remaining = self._remaining - e
+        return self._base_data[:e]
+
+    def remaining(self):
+        return self._remaining
+
+if len(sys.argv) == 1:
+    print('Usage: app <url> <size>')
+    exit(1)
+
+url = sys.argv[1]
+size = int(sys.argv[2]) if len(sys.argv) >= 3 else 1024
+
+print('=== Parameters ===')
+print(f'Url: {url}')
+print(f'Size: {size}')
+print()
+
+parsedUrl = urlparse(url)
+
+headers = {
+    "Content-Length": str(size),
+}
+
+conn = http.client.HTTPConnection(parsedUrl.netloc)
+
+while True:
+    start = time.perf_counter()
+    conn.request("PUT", parsedUrl.path, body=LargeStream(size))
+    resp = conn.getresponse()
+    resp.read()
+    stop = time.perf_counter()
+
+    duration = stop - start
+    mbps = ((size / duration) * 8) / (1024 * 1024)
+
+    print(f'Put {size:,} bytes in {duration:.2f} seconds ({mbps:.2f} Mbps)')
