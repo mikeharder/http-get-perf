87e490248e5cbef1e2362c5b4fb472478ee9465b
Graham Wheeler <gramster@users.noreply.github.com>
2023-01-20 12:22:28 -0800

    Add additional Python frameworks: httpx and blacksheep (#19)

---
+++ python-blacksheep-uvloop/app.py
@@ -0,0 +1,56 @@
+import asyncio
+import sys
+import time
+from blacksheep.client import ClientSession
+
+
+completed_requests = 0
+
+async def execute_requests(session, url):
+    global completed_requests
+    while True:
+        response = await session.get(url)
+        await response.text()
+        completed_requests += 1
+
+def print_results(requests, duration):
+    requests_per_second = requests / duration
+    print(f'Completed {requests:,} requests in {duration:.2f} seconds ({requests_per_second:,.0f} req/s)')
+    print()
+
+async def collect_results(title, duration):
+    global completed_requests
+    print(f'=== {title} ===')
+
+    start = time.perf_counter()
+    completed_requests = 0
+    await asyncio.sleep(duration)
+    end = time.perf_counter()
+
+    print_results(completed_requests, end - start)
+
+async def main():
+    if len(sys.argv) == 1:
+        print('Usage: app <url> <parallel> <warmup> <duration>')
+        return
+    
+    url = sys.argv[1]
+    parallel = int(sys.argv[2]) if len(sys.argv) >= 3 else 32
+    warmup = int(sys.argv[3]) if len(sys.argv) >= 4 else 10
+    duration = int(sys.argv[4]) if len(sys.argv) >= 5 else 10
+
+    print('=== Parameters ===')
+    print(f'Url: {url}')
+    print(f'Parallel: {parallel}')
+    print(f'Warmup: {warmup}')
+    print(f'Duration: {duration}')
+    print()
+
+    async with ClientSession() as session:
+        asyncio.gather(*[execute_requests(session, url) for i in range(parallel)])
+        await collect_results('Warmup', warmup)
+        await collect_results('Test', duration)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
