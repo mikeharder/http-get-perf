89ca21fc49803e568ff1c41ff16e13abb8a0347e
Mike Harder <mharder@microsoft.com>
2023-01-20 08:05:18 +0000

    Increase default connections to 64 for python-requests and python-aiohttp

--- python-aiohttp/app.py
+++ python-aiohttp/app.py
@@ -34,7 +34,7 @@ async def main():
         return
     
     url = sys.argv[1]
-    parallel = int(sys.argv[2]) if len(sys.argv) >= 3 else 32
+    parallel = int(sys.argv[2]) if len(sys.argv) >= 3 else 64
     warmup = int(sys.argv[3]) if len(sys.argv) >= 4 else 10
     duration = int(sys.argv[4]) if len(sys.argv) >= 5 else 10
 
